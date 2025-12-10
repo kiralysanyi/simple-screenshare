69eb96a7bd85092ab2b9e6c604e9015725966b12
Király Sándor <kiralysandor1986@gmail.com>
2025-12-10 16:44:24 +0100

    fix: fixed a bunch of code that shouldn't have worked in the first place, server reconnection problems

--- src/backend/index.js
+++ src/backend/index.js
@@ -29,6 +29,9 @@ io.on("connection", (socket) => {
 
         //remove empty rooms
         const cleanUp = () => {
+            if (rooms[roomid] == undefined) {
+                return;
+            }
             console.log("Socket disconnected")
             console.log(rooms[roomid])
             if (rooms[roomid]["hostpeer"] == undefined && rooms[roomid]["peers"].length == 0) {
@@ -77,6 +80,9 @@ io.on("connection", (socket) => {
             io.to(roomid).emit("newPeer", peerid)
 
             socket.on("disconnect", () => {
+                if (!rooms[roomid]) {
+                    return;
+                }
                 if (rooms[roomid]["hostsocket"]) {
                     rooms[roomid]["hostsocket"].emit("removePeer", peerid);
                 }
