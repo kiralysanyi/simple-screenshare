7db6a71e7ac3729e4a37d24e79e3028abde6d351
Király Sándor <kiralysandor1986@gmail.com>
2025-12-16 17:23:42 +0100

    - Added h264 because why not

--- frontend/src/Stream.tsx
+++ frontend/src/Stream.tsx
@@ -140,6 +140,30 @@ const Stream = () => {
                     }
                     break;
 
+                case "H264":
+                    options = {
+                        track: videoTrack,
+                        codec: {
+                            preferredPayloadType: 96,
+                            kind: 'video',
+                            mimeType: 'video/H264',
+                            clockRate: 90000,
+                            parameters: {
+                                'packetization-mode': 1,
+                                'profile-level-id': '42e01f',
+                                'level-asymmetry-allowed': 1
+                            },
+                            rtcpFeedback: [
+                                { type: 'nack' },
+                                { type: 'nack', parameter: 'pli' },
+                                { type: 'ccm', parameter: 'fir' },
+                                { type: 'goog-remb' },
+                                { type: 'transport-cc' }
+                            ]
+                        }
+                    }
+                    break;
+
                 default:
                     console.log("Defaulted back to VP9")
                     options = {
@@ -370,8 +394,9 @@ const Stream = () => {
                     <label htmlFor="codec">Codec</label>
                     <select name="codec" disabled={streamStarted} onChange={(ev) => { setCodec(ev.target.value) }}>
                         <option value="VP9">VP9 (Recommended)</option>
-                        <option value="VP8">VP8 (Recommended if one of the viewers recieve only blank stream)</option>
+                        <option value="VP8">VP8</option>
                         <option value="AV1">AV1</option>
+                        <option value="H264">H264</option>
                     </select>
                 </div>
                 <div className="form-group">
